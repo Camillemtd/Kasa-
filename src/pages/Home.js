086260811befde8532f14077cc745ad0e7930a88
Camillemtd <camille_mtd@hotmail.com>
2023-03-31 16:42:42 +0200

    Home + About

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import Header from '../component/Header';
-
+import img from "../assets/home.png"
+import "./stylesPages/home.scss"
+import datakasa from '../data/datakasa.json'
+import LocationCard from '../component/LocationCard';
+import Footer from '../component/Footer';
 const Home = () => {
+    
+    
+        
+              
     return (
-        <div>
+        <>
             <Header/>
-            home 
-        </div>
+            <main className='home'>
+                <div className='containerImgHome'>
+                    <img src={img} alt="falaise" className='imgHome' />
+                    <h2>Chez vous, partout et ailleurs</h2>
+                </div>
+                <article className='cardContainer'>
+                    {datakasa.map(e => {
+                        return(
+                            <LocationCard cover={e.cover} title={e.title} key={e.id}/>
+                        )
+                    })}
+                </article>
+            </main>
+            <Footer/>
+        </>
     );
 };
 
